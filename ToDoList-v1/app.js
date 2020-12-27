366df926854cf268c2faded82b9e15f05b844d2e
lyndoncortez <lyndoncortez@yahoo.com>
2020-12-28 00:33:55 +0800

    Passing Data from Webpage to Server

--- ToDoList-v1/app.js
+++ ToDoList-v1/app.js
@@ -1,49 +1,43 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require(__dirname + "/date.js");
 
 const app = express();
+
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
+    let day = date.getDate();
 
-  if (currentDay === 6 || currentDay === 0) {
-      day = "Weekend";
-  } else {
-      day = "Weekday";
-  }
+    res.render("list", {
+        listTitle: day,
+        newListItems: items
+    });
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        default:
-        console.log("Error: current day is equals to: " + currentDay);
+});
+
+app.post("/", function(req, res) {
+    let item = req.body.newItem;
+
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
     }
+    
+    
+});
 
-  res.render("list", {kindOfDay: day})
-
+app.get("/work", function(req, res) {
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
 
 app.listen(3000, () => console.log('Server started on port 3000!'));
